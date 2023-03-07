54a1ed5eaafd987d1a427d6a81579edbb559b52c
Arshad Khan <khanarshad0003@outlook.com>
2023-03-07 12:24:35 +0530

    third commit

---
+++ src/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>PHP</title>
+</head>
+<body>
+<script type="text/javascript">
+	alert("Hii welcome to site...");
+</script>
+</body>
+</html>
